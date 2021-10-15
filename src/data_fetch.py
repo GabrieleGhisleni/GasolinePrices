a87cebee53e8375c428d869f7dc43f590d52f4c1
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-10-15 22:23:35 +0200

    add refresh stations

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -22,3 +22,13 @@ def retrive_stations_and_merge(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     except Exception as e:
         print(f"""Something went wrong while loading the static impianti,geojson, {e}
                 Current working directory {os.getcwd()})""")
+
+def refresh_stations() -> None:
+    url_impianti = "https://www.mise.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv"
+    df_impianti = pd.read_csv(url_impianti, delimiter=';', skiprows=1)
+    geometry = gpd.points_from_xy(df_impianti.Longitudine, df_impianti.Latitudine)
+    geo_stations = gpd.GeoDataFrame(df_impianti,crs='EPSG:4326', geometry=geometry)
+    geo_stations = geo_stations.loc[:, ['idImpianto', 'Gestore','Bandiera', 'Nome Impianto','Indirizzo', 'Comune', 'geometry']]
+    geo_stations= geo_stations.loc[geo_stations.geometry.is_valid,:]
+    geo_stations.rename(columns={'idImpianto': 'id'}, inplace=True)
+    geo_stations.to_file('./data/impianti.geojson', driver='GeoJSON')  
--- src/main.py
+++ src/main.py
@@ -1,19 +1,29 @@
 from data_fetch import *
 from data_process import *
 import datetime as dt
+import argparse
 
 
 def main():
     def time_(): return dt.datetime.now().strftime("%S:%M %d-%m-%Y")
-    start = dt.datetime.now()
-    print(f'Started at {time_()}')
-    retrived_prices = retrive_prices()
-    geo_df = retrive_stations_and_merge(retrived_prices)
-    print(f'Retrived all the information, start processing at {time_()}')
-    storage = process(geo_df)
-    print(f'Retrived all the information, start saving at {time_()}')
-    save(storage)
-    print(f'finished at {time_()}, total time={dt.datetime.now() - start}')
+    arg_parse = argparse.ArgumentParser(description='Argument for GitHub Actions')
+    arg_parse.add_argument('-n', '--new', action='store_true')
+    arg_parse.add_argument('-d', '--default', action='store_true')
+    args = arg_parse.parse_args()
+
+    if args.default:
+        start = dt.datetime.now()
+        print(f'Started at {time_()}')
+        retrived_prices = retrive_prices()
+        geo_df = retrive_stations_and_merge(retrived_prices)
+        print(f'Retrived all the information, start processing at {time_()}')
+        storage = process(geo_df)
+        print(f'Retrived all the information, start saving at {time_()}')
+        save(storage)
+        print(f'finished at {time_()}, total time={dt.datetime.now() - start}')
+    
+    if args.new:
+        refresh_stations()
 
 
 if __name__ == '__main__':
