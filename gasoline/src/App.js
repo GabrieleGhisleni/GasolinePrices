898d1419d420721573efc8d9ae9c233222bb6acc
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-10-16 15:27:20 +0200

    minor bug

--- gasoline/src/App.js
+++ gasoline/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Main from './components/Main';
 
 function App() {
--- src/data_fetch.py
+++ src/data_fetch.py
@@ -8,7 +8,7 @@ def retrive_prices() -> pd.DataFrame:
         url = "https://www.mise.gov.it/images/exportCSV/prezzo_alle_8.csv"
         df = pd.read_csv(url, delimiter=';', skiprows=1)
         df = df.loc[:, ['idImpianto', 'descCarburante', 'prezzo', 'dtComu']]
-        df.columns = ['id', 'gasoline_type','price']
+        df.columns = ['id', 'gasoline_type','price', 'date']
         return df
     except Exception as e:
         print(f'Something went wrong while fetching {url}, {e}')
